2d8404903135d8e32a1439329166b4d1ab2f3bbc
Dalton <daltondheising@gmail.com>
2024-11-21 18:31:48 -0500

    Added cards by max level

--- ClashRoyaleData.py
+++ ClashRoyaleData.py
@@ -39,6 +39,7 @@ with open("ClashRoyaleView.json", "r", encoding="utf-8") as json_file:
 # Extract card names and elixir costs
 card_names = []
 elixir_costs = []
+maxed_level = []
 
 # Dictionaries to store totals
 rarity_card_counts = {}
@@ -49,7 +50,7 @@ for card in cards_data['items']:
     elixir_costs.append(card.get('elixirCost', 0))  # Some cards may not have elixir cost, use 0 as default
     rarity = card.get('rarity', 'Unknown')
     evolution_level = card.get('maxEvolutionLevel', 0)
-    
+    maxed_level.append(card.get('maxLevel', 0))
     # Update card count for this rarity
     rarity_card_counts[rarity] = rarity_card_counts.get(rarity, 0) + 1
     
@@ -70,7 +71,6 @@ elixir_costs_sorted, card_names_sorted= zip(*cards_sorted)
 # Create a vertical bar chart using matplotlib
 fig, ax1 = plt.subplots(figsize=(10, 6))
 ax1.bar(card_names_sorted, elixir_costs_sorted, color='lightskyblue')
-
 # Rotate the card names on the x-axis for readability
 ax1.set_xticklabels(card_names_sorted, rotation=90, fontsize=10)
 
@@ -91,6 +91,14 @@ ax3.bar(rarities, evolution_totals, color='orange')
 ax3.set_title('Total Evolutions by Rarity', fontsize=16)
 ax3.set_xlabel('Rarity', fontsize=12)
 ax3.set_ylabel('Total Evolution Levels', fontsize=12)
+
+fig, ax4 = plt.subplots(figsize=(10, 6))
+ax4.bar(card_names, maxed_level, color='purple')
+# Rotate the card names on the x-axis for readability
+ax4.set_xticklabels(card_names, rotation=90, fontsize=10)
+ax4.set_title('Cards by Maxed Level ', fontsize=16)
+ax4.set_xlabel('Card Name', fontsize=12)
+ax4.set_ylabel('Maxed Level', fontsize=12)
 
 # Display the plot
 plt.tight_layout()
